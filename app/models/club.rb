6797e14253b8ae1d978af6b4b47a32e1ba5933d2
maggie-bingham <mbingham4309@gmail.com>
2015-09-30 09:52:08 -0400

    final

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,2 +1,7 @@
 class Club < ActiveRecord::Base
+validates :name, length: {maximum: 30}
+
+has_many :memberships
+has_many :users, through: :memberships
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+validates :first_name, uniqueness: true
+validates :last_name, uniqueness: true
+
+has_many :memberships
+has_many :clubs, through: :memberships
+
 end
